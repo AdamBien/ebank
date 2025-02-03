bc81552c27cc797ac8f1a0e2d26d2425f7781f2b
Adam Bien <abien@adam-bien.com>
2025-02-03 17:37:59 +0100

    AccountCreationResult is sealed

--- ebank/src/main/java/airhacks/ebank/accounting/control/AccountCreationResult.java
+++ ebank/src/main/java/airhacks/ebank/accounting/control/AccountCreationResult.java
@@ -2,9 +2,13 @@ package airhacks.ebank.accounting.control;
 
 import airhacks.ebank.accounting.entity.Account;
 
-public sealed interface AccountCreationResult {
+public sealed interface AccountCreationResult permits AccountCreationResult.AlreadyExists,
+        AccountCreationResult.Invalid,
+        AccountCreationResult.Created {
+            
     record AlreadyExists(Account account) implements AccountCreationResult {
     }
+
     record Invalid(Account account) implements AccountCreationResult {
     }
 
