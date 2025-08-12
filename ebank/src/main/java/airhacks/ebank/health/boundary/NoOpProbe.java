c81d2c726938fc85a824fb50ea162b0e8c48b67a
Adam Bien <abien@adam-bien.com>
2025-08-12 20:57:17 +0200

    healthcheck access logged

--- ebank/src/main/java/airhacks/ebank/health/boundary/NoOpProbe.java
+++ ebank/src/main/java/airhacks/ebank/health/boundary/NoOpProbe.java
@@ -1,17 +1,25 @@
 package airhacks.ebank.health.boundary;
 
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @Liveness
 @ApplicationScoped
 public class NoOpProbe implements HealthCheck {
 
+    @Inject
+    Logger log;
+
     @Override
     public HealthCheckResponse call() {
+        log.log(Level.INFO, "liveness checked");
         return HealthCheckResponse
                 .up("Basic Availability");
     }
